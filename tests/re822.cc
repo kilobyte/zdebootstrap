717c5f1e6cf47f7c2c9618b8705ffc7bc7066b9c
Adam Borowski <kilobyte@angband.pl>
2019-04-26 23:46:33 +0200

    Some tests for the 822 parser.

---
+++ tests/re822.cc
@@ -0,0 +1,19 @@
+#include "../822.h"
+#include <unistd.h>
+
+int main()
+{
+    std::string txt;
+    char buf[4096];
+    ssize_t r;
+
+    while ((r = read(0, buf, sizeof(buf))) > 0)
+        txt.append(buf, r);
+    if (r<0)
+        return perror("re822: read"), 1;
+
+    deb822 s(txt.c_str());
+    s.fprint(stdout);
+
+    return 0;
+}
